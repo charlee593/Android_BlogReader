27578afdcef5cf2f09dfcaacb2f98ffe6bfc81d4
charlee593 <charlee593@gmail.com>
2015-01-25 00:18:31 -0500

    Add activity that run the browser intent

--- app/src/main/java/com/example/android/blogreader/MainListActivity.java
+++ app/src/main/java/com/example/android/blogreader/MainListActivity.java
@@ -75,7 +75,8 @@ public class MainListActivity extends ListActivity {
             jsonPosts = mBlogData.getJSONArray("posts");
             JSONObject jsonPost = jsonPosts.getJSONObject(position);
             String blogUrl = jsonPost.getString("url");
-            Intent intent = new Intent(Intent.ACTION_VIEW);
+
+            Intent intent = new Intent(this, BlogWebViewActivity.class);
             intent.setData(Uri.parse(blogUrl));
             startActivity(intent);
         } catch (JSONException e) {
